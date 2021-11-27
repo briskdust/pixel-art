eaeab41632a4274584ab15de4996b8138d82083f
briskdust <briskdust@gmail.com>
2021-11-27 21:13:29 +0100

    finished version

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
+let button = document.querySelector('input[type="submit"]');
+let table = document.getElementById("pixelCanvas");
+let color = document.getElementById("colorPicker").value;
+let height;
+let width;
 
-// When size is submitted by the user, call makeGrid()
+let getNum = () => {
+    height = parseInt(document.getElementById("inputHeight").value);
+    width = parseInt(document.getElementById("inputWidth").value);
+};
+
+let getColor = () => {
+    color = document.getElementById("colorPicker").value;
+};
+
+button.addEventListener("click", event => {
+    event.preventDefault(); // To prevent automatical reload of the web page
+    makeGrid();
+});
 
 function makeGrid() {
-
-// Your code goes here!
-
+    for (let i = 0; i < height; i++) {
+        let temp = document.createElement("tr");
+        for (let j = 0; j < width; j++) {
+            let cell = document.createElement("td");
+            cell.addEventListener("click", event => {
+                cell.style.backgroundColor = color;
+            });
+            temp.appendChild(cell);
+        }
+        table.appendChild(temp);
+    }
 }
